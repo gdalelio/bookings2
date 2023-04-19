828addcef873983ff39a4dd78158a2f13e84db9c
Geoff d'Alelio <geoff.dalelio@mac.com>
2023-04-18 23:07:56 -0400

    Post lecture 68 Server Side Validation III

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -36,18 +36,11 @@ func NewHandlers(r *Repository) {
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	/* stringMap := make(map[string]string)
-	stringMap["test"] = "Hello Again"
-
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP */
-
 	//send the data to the template
 	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
 		//StringMap: stringMap,
@@ -56,24 +49,18 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Generals is the room page handler
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
 }
 
 // Majors is the room page handler
 func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, r, "majors.page.tmpl", &models.TemplateData{})
 }
 
 // CheckAvailability is the check availability page handler
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
 }
@@ -111,8 +98,18 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 
 // Reservation is the check availability page handler
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	//create var to hold the empty reservation type
+	var emptyReservation models.Reservation
+
+	//create a map to store the entries on the form
+	data := make(map[string]interface{})
+
+	//store the data into the emptyReservation
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 
 }
@@ -133,11 +130,14 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	}
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email")
+
+	form.MinLength("first_name", 3, r)
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
 		data["reservation"] = reservation
+
 		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 			Form: form,
 			Data: data,
